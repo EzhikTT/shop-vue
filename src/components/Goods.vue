9f594dc803be6604f579e7ba46498460e29bb4c4
Alexandr Petruhin <sprf8611@gmail.com>
2019-10-06 14:07:04 +0300

    Styling goods

--- src/components/Goods.vue
+++ src/components/Goods.vue
@@ -1,13 +1,42 @@
 <template>
-    
+    <div class="goods">
+        <GoodsItem v-for="(item, id) in items"
+                   :item="item"
+                   :classProp="id % 6 === 0 ? 'two-row' : ''">
+        </GoodsItem>
+    </div>
 </template>
 
 <script>
+    import GoodsItem from './GoodsItem';
+
     export default {
-        name: "Goods"
+        name: "Goods",
+        components: {GoodsItem},
+        data(){
+            return {
+                items: []
+            };
+        },
+        mounted(){
+            fetch(
+                'http://192.168.64.2/api/subcategory.php',
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            ).then(res => res.json()).then(res => {
+                this.items = res;
+            });
+        }
     }
 </script>
 
 <style scoped>
-
+    .goods{
+        display: flex;
+        flex-wrap: wrap;
+    }
 </style>
